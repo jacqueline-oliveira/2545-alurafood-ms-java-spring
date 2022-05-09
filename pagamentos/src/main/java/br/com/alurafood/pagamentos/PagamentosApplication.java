0c4734d216842f9eacf24f497a89005d0217b67c
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2022-05-08 21:01:24 -0300

    Aula 01 projeto inicial

---
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.alurafood.pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentosApplication.class, args);
+	}
+
+}
