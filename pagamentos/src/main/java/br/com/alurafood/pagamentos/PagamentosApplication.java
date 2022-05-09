65827ba90a77121a2dad7b9cad1ab84382a0a5cb
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2022-05-09 20:03:57 -0300

    video 4.2 incluindo dependência do eureka no ms de pagamentos

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
@@ -2,8 +2,11 @@ package br.com.alurafood.pagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+
 public class PagamentosApplication {
 
 	public static void main(String[] args) {
