958cb3c3247307c2cdf1d8a5a2cbebf4cf3f1cc8
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2022-05-10 00:17:24 -0300

    video 5.2 balanceamento de carga

--- pedidos/src/main/java/br/com/alurafood/pedidos/controller/PedidoController.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/controller/PedidoController.java
@@ -4,6 +4,7 @@ import br.com.alurafood.pedidos.dto.PedidoDto;
 import br.com.alurafood.pedidos.dto.StatusDto;
 import br.com.alurafood.pedidos.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -30,6 +31,11 @@ public class PedidoController {
             PedidoDto dto = service.obterPorId(id);
 
             return  ResponseEntity.ok(dto);
+        }
+
+        @GetMapping("/porta")
+        public String retornaPorta(@Value("${local.server.port}") String porta){
+            return String.format("Requisição respondida pela instância executando na porta %s", porta);
         }
 
         @PostMapping()
--- pedidos/src/main/java/br/com/alurafood/pedidos/dto/PedidoDto.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/dto/PedidoDto.java
@@ -19,4 +19,5 @@ public class PedidoDto {
     private List<ItemDoPedidoDto> itens = new ArrayList<>();
 
 
+
 }
